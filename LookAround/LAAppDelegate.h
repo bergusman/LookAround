3de83fdde443597f1009af92d8ea1d620ca7a43d
Vitaliy Berg <bergusman@gmail.com>
2013-07-03 00:35:57 +0400

    Moving project tree

--- LookAround/LAAppDelegate.h
+++ LookAround/LAAppDelegate.h
@@ -8,12 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@class LAViewController;
-
 @interface LAAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
-@property (strong, nonatomic) LAViewController *viewController;
 
 @end
